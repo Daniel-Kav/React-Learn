9684e0d7c887cbf0ff823d2d44d9772bd2c9f20f
daniel-kav <dankavatha01@gmail.com>
2024-06-23 17:43:58 +0300

    blogging

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -5,19 +5,30 @@ import { useEffect } from 'react'
 
 function App() {
   const [blogs , setBlogs] = useState(null)
+  const [isPending, setisPending] = useState(true)
 
   useEffect(()=> {
-    fetch('http://localhost:8000/blogs')
+    setTimeout(()=>{
+      fetch('http://localhost:8000/blogs')
     .then(res => {
       return res.json();
     })
-    .then( data => setBlogs(data) )
+  .then( data => {
+    setBlogs(data)
+    setisPending(false)
+  })
     .catch(err => console.log(err) );
+    }, 1000)
   },[])
+useEffect(() => {
+    document.title = 'Blog Using React';
+}, []);
+
 
   return (
     <>
     {/* <NavBar /> */}
+    {isPending && <div>Loading ...</div>}
     {blogs && < BlogList blogs={blogs}/>}
     </>
   )
