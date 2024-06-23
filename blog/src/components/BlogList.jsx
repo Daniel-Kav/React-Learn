777d597016e54f72cd782c3be76c48b1af65216e
daniel-kav <dankavatha01@gmail.com>
2024-06-23 12:09:48 +0300

    bloglist components

--- blog/src/components/BlogList.jsx
+++ blog/src/components/BlogList.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-function BlogList() {
+function BlogList({ blogs }) {
   return (
-    <div>
-      
+    <div className="list">
+        {blogs.map((blog) => (
+            <div className="blog" key={blog.id}>
+                <h3>{blog.title}</h3>
+                <p>Written By {blog.author}</p>
+            </div>
+        ))}
     </div>
   )
 }
