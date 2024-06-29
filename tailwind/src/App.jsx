039d6b20a17e3b71b43292af6c7495434b764ac7
daniel-kav <dankavatha01@gmail.com>
2024-06-29 16:42:51 +0300

    set up a simple page styled using tailwind

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Button from './components/Button';
 
 const App = () => {
+  const number = 40
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-lg">
+    <div className="min-h-screen flex items-center  justify-center p-8 bg-blue-300">
+      <div className="bg-white p-6 rounded-lg shadow-lg ">
         <h1 className="text-2xl font-bold mb-4">Welcome to Vite + React + Tailwind CSS</h1>
-        <p className="mb-4">This is a simple example of using Tailwind CSS with a Vite React project.</p>
-        <Button text="Click me!" />
+        <p className="mb-4 ">This is a simple example of using Tailwind CSS with a Vite React project.</p>
+        <Button text="Click me!"/>
       </div>
     </div>
   );
--- tailwind/src/components/Button.jsx
+++ tailwind/src/components/Button.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 
 
-const Button = ({ text }) => {
+const Button = ({ text , num }) => {
   return (
-    <button className=" bg-red-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <button className=" bg-red-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ">
       {text}
+      {num}
     </button>
   );
 };
