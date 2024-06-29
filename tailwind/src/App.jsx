15af4960a4f1bad36be76d86a3c667479bd2804e
daniel-kav <dankavatha01@gmail.com>
2024-06-29 16:57:46 +0300

    added daisy ui and a nabar template

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Button from './components/Button';
+import NavBar from './components/NavBar';
 
 const App = () => {
   const number = 40
   return (
+    <>
+    < NavBar />
     <div className="min-h-screen flex items-center  justify-center p-8 bg-blue-300">
+      
       <div className="bg-white p-6 rounded-lg shadow-lg ">
         <h1 className="text-2xl font-bold mb-4">Welcome to Vite + React + Tailwind CSS</h1>
         <p className="mb-4 ">This is a simple example of using Tailwind CSS with a Vite React project.</p>
         <Button text="Click me!"/>
       </div>
     </div>
+    </>
   );
 };
 
