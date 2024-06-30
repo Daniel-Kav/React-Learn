162369def006d295f7bddf04f229e1118635ced3
daniel-kav <dankavatha01@gmail.com>
2024-06-30 13:11:18 +0300

    changed the nav

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from './components/Button';
 import NavBar from './components/NavBar';
+import Card from './components/Card';
 
 
 
@@ -9,14 +10,6 @@ const App = () => {
   return (
     <>
     < NavBar />
-    <div className="min-h-screen flex items-center  justify-center p-8 bg-blue-300">
-      
-      <div className="bg-white p-6 rounded-lg shadow-lg ">
-        <h1 className="text-2xl font-bold mb-4">Welcome to Vite + React + Tailwind CSS</h1>
-        <p className="mb-4 ">This is a simple example of using Tailwind CSS with a Vite React project.</p>
-        <Button />
-      </div>
-    </div>
     </>
   );
 };
