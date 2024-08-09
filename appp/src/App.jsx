f4d8eca47f141402750bc822b88a6ebbdee13727
daniel-kav <dankavatha01@gmail.com>
2024-08-09 22:22:45 +0300

    message form

--- appp/src/App.jsx
+++ appp/src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./Pages/Home";
-// import Appointment from "./Pages/Appointment";
-// import AboutUs from "./Pages/AboutUs";
-// import Register from "./Pages/Register";
-// import Footer from "./components/Footer";
+import Home from "./Pages/Home";
+import Appointment from "./Pages/Appointment";
+import AboutUs from "./Pages/AboutUs";
+import Register from "./Pages/Register";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
